25d2e1cfd463b73b776335d6f2f3ea86725001e0
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-01-30 16:34:58 -0300

    #Add: agregar productos, editar productos, eliminar productos, crear categorias, se creo el index principal

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from . import views
 
+
+app_name = 'inventario'
+
+
+
 urlpatterns = [
+    path('', views.index, name="index"),
     path('create_producto/', views.create_producto, name="crear_producto"),
+    path('edit_producto/<int:id_producto>', views.edit_producto, name="editar_producto"),
+    path('eliminar/<int:producto_id>/', views.delete_producto, name='eliminar_producto'),
     path('create_categoria/', views.create_categoria, name="crear_categoria"),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,26 +1,79 @@
-from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 from inventario.forms import CategoriasForm, ProductoForm
+from inventario.models import Producto, Categorias
 
+
+def index(request):
+    
+    productos_list = Producto.objects.all()  
+
+    codigo = request.GET.get('codigo', '')
+    nombre = request.GET.get('nombre', '')
+    categoria_id = request.GET.get('categoria', '')
+
+    if codigo:
+        productos_list = productos_list.filter(codigo=codigo)
+    if nombre:
+        productos_list = productos_list.filter(nombre__icontains=nombre)
+    if categoria_id:
+        productos_list = productos_list.filter(categoria_id=categoria_id)
+
+    paginator = Paginator(productos_list, 6)  
+    page_number = request.GET.get('page')
+    productos = paginator.get_page(page_number)
+
+    categorias = Categorias.objects.all() 
+    return render(request, 'inventario/productos/index.html', {
+        'productos': productos,
+        'categorias': categorias,
+        'codigo': codigo,
+        'nombre': nombre,
+        'categoria_id': categoria_id
+    })
 
 def create_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('crear_producto')  
+            messages.success(request, ("Producto creado correctamente!"))
+            return redirect('inventario:crear_producto')  
     else:
         form = ProductoForm()
     return render(request, 'inventario/productos/create.html', {'form': form})
     
+    
+def edit_producto(request, id_producto):
+    producto = get_object_or_404(Producto, id=id_producto)  
+
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Producto editado correctamente!"))
+            return redirect('inventario:index')  
+    else:
+        form = ProductoForm(instance=producto)
+
+    return render(request, 'inventario/productos/edit.html', {'form': form, 'producto': producto})
+
+def delete_producto(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    producto.delete()
+    messages.success(request, "Producto eliminado correctamente.")
+    return redirect('inventario:index') 
 
 def create_categoria(request):
     if request.method == 'POST':
         form = CategoriasForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('create_categorias')
+            messages.success(request, ("Categoría creada correctamente!"))
+            return redirect('inventario:create_categorias')
     else:
         form = CategoriasForm()
     return render(request, 'inventario/categorias/create.html', {'form': form})
