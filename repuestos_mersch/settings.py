eba78524893d3c4000599f5c9e376aba7bd228ce
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-02-05 13:27:55 -0300

    Add: ajustes deploy

--- repuestos_mersch/settings.py
+++ repuestos_mersch/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'repuestos_mersch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "voCCFRWjOFTIIYufmPNCEnKGUoexlxuD",
+        "HOST": "postgres.railway.internal",
+        "PORT": "5432",
     }
 }
 
