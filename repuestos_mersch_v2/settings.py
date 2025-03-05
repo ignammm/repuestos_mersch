a49fc7d59ccc7c04d4a07976c65bf4127c3c3b3f
Ignacio Mersch <ignaciomersch602811@gmail.com>
2025-03-05 10:57:15 -0300

    #Deploy ajustes

--- repuestos_mersch_v2/settings.py
+++ repuestos_mersch_v2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://repuestos-mersch.onrender.com']
+ALLOWED_HOSTS = [os.getenv('HOST_DB'),]
 
 
 # Application definition
